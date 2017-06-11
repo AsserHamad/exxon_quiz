62748a08ff56f8b2452d3b02b76e5878a4210246
moar55 <moar.ahmed@gmail.com>
2017-06-11 16:21:45 +0200

    runs

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -11,8 +11,7 @@ router.get('/', function (req, res, next) {
   Article.find(function (err, articles) {
     if (err) return next(err);
     res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
+      title: 'Generator-Express MVC'
     });
   });
 });
