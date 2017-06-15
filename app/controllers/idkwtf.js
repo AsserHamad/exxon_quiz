652ffdc65c604fab4e71ff0020e82436ae85f53b
AsserHamad <asserhamad96@gmail.com>
2017-06-15 09:27:18 +0200

    Changed login page to now include the logging in functionality, however registering will be implemented shortly.

--- app/controllers/idkwtf.js
+++ app/controllers/idkwtf.js
@@ -1,15 +1,5 @@
 
 $(document).ready(function(){
   var glowy = false;
-  setInterval(function(){
-    console.log("Glowy is "+glowy);
-    if(glowy){
-      glowy=false;
-      $("#logo").addClass("glowy");
-    }
-    else{
-      glowy = true;
-      $("#logo").removeClass("glowy");
-    }
-  },3000);
+  setTimeout(function(){$("#logo").removeClass("glowy");},1000)
 });
--- public/js/index_ctrl.js
+++ public/js/index_ctrl.js
@@ -1,15 +1,4 @@
 $(document).ready(function(){
   var glowy = false;
-  setInterval(function(){
-    console.log("Glowy is "+glowy);
-    console.log("Classes: "+$("#logo").attr("class"));
-    if(glowy){
-      glowy=false;
-      $("#logo").addClass("glowing");
-    }
-    else{
-      glowy = true;
-      $("#logo").removeClass("glowing");
-    }
-  },1000);
+  setTimeout(function(){$("#logo").removeClass("glowing");},1000)
 });
