297f93ed0b73b195ceaaeee285e1bedcc96d91c8
Abdurrahmna95 <abdurrahman.maged@gmail.com>
2017-06-16 15:52:01 +0200

    added title to the landing page and animated it, and some minor stylings

--- public/js/index_ctrl.js
+++ public/js/index_ctrl.js
@@ -1,26 +1,40 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   $("#login_modal").on('keypress', (e) => {
-    if(e.which == 13)
+    if (e.which == 13)
       $("#login_btn").click();
-  });
+    }
+  );
 
   var glowy = false;
-  setTimeout(function(){$("#logo").removeClass("glowing");},1000);
+  setTimeout(function() {
+    $("#logo").removeClass("glowing");
+  }, 1000);
 
-  $("#login_btn").click(function(){
+  $("#login_btn").click(function() {
     const email = $("#login_email").val(),
-          password = $("#login_password").val(),
-          credentials = {email: email, password: password}
+      password = $("#login_password").val(),
+      credentials = {
+        email: email,
+        password: password
+      }
 
     $.post('/signin', credentials, (res) => {
       console.log(res);
       $("#login_error").empty();
-      if(res.success)
-      window.location = '/';
-      else if(res.info || res.err) {
+      if (res.success)
+        window.location = '/';
+      else if (res.info || res.err) {
         $("#login_error").html(res.info.message || JSON.stringify(res.err));
       }
     })
   })
+  $('.quizmania').funnyText({
+  		speed: 400,
+  		borderColor: 'none',
+  		activeColor: 'white',
+  		color: 'white',
+  		fontSize: '9em',
+  		direction: 'both'
+  	});
 });
