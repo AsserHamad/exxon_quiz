992b490587b7ce8b8b34fed7a0cd0cd1cb897390
AsserHamad <asserhamad96@gmail.com>
2017-07-03 05:36:39 +0200

    Quiz started taking shape

--- public/js/home_ctrl.js
+++ public/js/home_ctrl.js
@@ -1,12 +1,13 @@
+var questions;
 $('document').ready(function(){
   window.onbeforeunload = function() {
   return "Data will be lost if you leave the page, are you sure?";
 };
-  var questions;
+  var topScore;
   if(user)$("#admin").css('display',(user.role=='admin')?'inline' :'none');
   $("#button_single").on('click',function(){
     $("#button_multi").animate({opacity: 0, width: 0, display:'none'}, 500);
-    $("#clicks").animate({marginTop:'10rem',width: '80%'},1000);
+    $("#clicks").animate({marginTop:'3rem',width: '80%'},1000);
     $("#button_single").css('animation','single_transition 2s forwards');
      setTimeout(() => {
       $("#single_match").css('animation','show_quiz 2s forwards');
@@ -19,5 +20,20 @@ $('document').ready(function(){
     questions = res;
   }).fail((err) => {
     alert('BEEP BOOP ERROR '+err);
+  })
+  $.get('/users/'+user._id+'/topscore')
+  .done((res) => {
+    $("#yourTopScore").html(Math.floor(res.score));
+    console.log('Your actual top score is '+res.score);
+  })
+  .fail((err) => {
+    topScore = 0;
+    console.log('Your filthy non-existent top score is '+topScore+' with error '+err);
+  })
+
+  $("#submit_btnn").on('click',() => {
+    
+    $("#single_match").html(JSON.stringify(questions));
+
   })
 });
